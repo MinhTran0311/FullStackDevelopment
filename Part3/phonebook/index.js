f807bfadf97353c3584dc1ba0567b64c54e414c2
MinhTran0311 <minhhappy0311@gmail.com>
2024-06-29 14:12:52 +0700

    Node.js and Express

---
+++ Part3/phonebook/index.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :body",
+    {
+      skip: (req, res) => req.method !== "POST",
+    }
+  )
+);
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.end(JSON.stringify(persons));
+});
+
+app.get("/api/info", (request, response) => {
+  var datetime = new Date();
+  var options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "short",
+  };
+  var formattedDate = datetime.toLocaleString("en-US", options);
+
+  response.send(
+    `<div> 
+        <p>Phone has info for ${persons.length} people</p> 
+        <p>${formattedDate}</p>
+    </div>`
+  );
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  if (persons.some((p) => p.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: String(Math.floor(Math.random() * 1000000)),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
