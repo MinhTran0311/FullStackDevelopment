6b760de939ec3eacca4453332532a5676ea33d49
MinhTran0311 <minhhappy0311@gmail.com>
2024-07-01 23:43:22 +0700

    writing unit tests

---
+++ Part4/bloglist/utils/list_helper.js
@@ -0,0 +1,72 @@
+const _ = require("lodash");
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const favorite = blogs.reduce(
+    (max, blog) => (blog.likes > max.likes ? blog : max),
+    blogs[0]
+  );
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const authorCounts = _.countBy(blogs, "author");
+  const topAuthor = _.maxBy(
+    _.keys(authorCounts),
+    (author) => authorCounts[author]
+  );
+
+  return {
+    author: topAuthor,
+    blogs: authorCounts[topAuthor],
+  };
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const authorLikes = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + blog.likes;
+    return acc;
+  }, {});
+
+  const topAuthor = _.maxBy(
+    _.keys(authorLikes),
+    (author) => authorLikes[author]
+  );
+
+  return {
+    author: topAuthor,
+    likes: authorLikes[topAuthor],
+  };
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
